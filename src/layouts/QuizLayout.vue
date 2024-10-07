eaa41db07c71d044b68be914e601241392a00d74
Kathrin <kathrin.ayer@gmail.com>
2024-10-07 08:32:15 -0400

    add basic particle quiz

--- src/layouts/QuizLayout.vue
+++ src/layouts/QuizLayout.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import AppHeader from "@/components/AppHeader.vue";
+import AppHeader from "@/components/QuizzesHeader.vue";
 </script>
 
 <template>
-  <div>
-    <h3>Here i am laying out</h3>
+  <main>
     <AppHeader />
     <RouterView />
-  </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+nav::v-deep {
+  flex: 0 0 15ch;
+}
+
+main > :nth-child(2) {
+  flex-basis: calc(100% - 15ch - 1rem);
+}
+</style>
--- src/pages/quizzes/quizzes/particle-quiz.vue
+++ src/pages/quizzes/quizzes/particle-quiz.vue
@@ -1,1 +1,94 @@
-<template>particle QUIZ</template>
+<script setup lang="tsx">
+import germanSeparableVerbs from "@constants/germanSeparable.json";
+import { computed, ref } from "vue";
+
+import {
+  RoundGenerator,
+  type ResponseObject,
+  type SeparableVerb,
+} from "./internal/round";
+
+const data: Record<string, SeparableVerb> = germanSeparableVerbs;
+const keys = Object.keys(data);
+
+const answered = ref(false);
+let responseWord = "";
+
+let roundObject = ref<ResponseObject | undefined>(undefined);
+
+const TestResponse = computed(() => {
+  if (!answered || roundObject === undefined || !roundObject.value) return "";
+
+  if (responseWord === roundObject.value?.testWord) {
+    return (
+      <>
+        <h3>Correct!</h3>
+        <p>
+          <span class="particle">{roundObject.value.particle}</span>
+          {roundObject.value.wordBase} means {roundObject.value.testDefinition}
+        </p>
+      </>
+    );
+  } else {
+    const definition = data[responseWord].translations.en;
+    return (
+      <>
+        <h3>Incorrect</h3>
+        <p>
+          {roundObject.value.testWord} means {roundObject.value.testDefinition}
+        </p>
+      </>
+    );
+  }
+});
+
+function handleClick(buttonValue: string) {
+  answered.value = true;
+  responseWord = buttonValue;
+}
+
+function nextRound() {
+  answered.value = false;
+  roundObject.value = RoundGenerator();
+  console.log(roundObject.value);
+}
+
+nextRound();
+</script>
+
+<template>
+  <div v-if="roundObject">
+    <TestResponse v-if="answered" />
+    <p>meaning: {{ roundObject.testDefinition }}</p>
+    <button
+      v-for="option in roundObject.currentOptions"
+      @click.stop="handleClick(option)"
+      :disabled="answered"
+    >
+      {{ option }}
+    </button>
+    <button v-if="answered" @click.stop="nextRound">Next</button>
+  </div>
+</template>
+
+<style scoped>
+.particle {
+  color: red;
+}
+
+div {
+  max-width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+
+  & p {
+    flex-basis: 100%;
+  }
+
+  & button {
+    padding: 0.75rem;
+    display: inline-block;
+  }
+}
+</style>
